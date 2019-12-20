e421bcd099fc2bfcbc393f2a8b320328f6844ed4
Daniel Barben <daniel.barben@tamedia.ch>
2019-12-20 19:55:40 +0100

    image first, headline, text

--- src/Components/Event/index.js
+++ src/Components/Event/index.js
@@ -21,8 +21,7 @@ open = () => {
     <li>
       <div className = "date" onClick = {() => this.open()}>{this.props.date} {this.state.visibility ?  <FaCaretUp /> : <FaCaretDown />}
       <h3 className = "headline">{this.props.headline}</h3></div>
-      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.text}<br/><a href={this.props.link}>{this.props.link !== "" ? "... zum Artikel" : ""}</a><br/>
-      {this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}
+      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}{this.props.text}<br/><a href={this.props.link} target="_blank">{this.props.link !== "" ? "... zum Artikel" : ""}</a>
       </p>
     </li>
   )}
--- src/Components/img/index.js
+++ src/Components/img/index.js
@@ -4,15 +4,18 @@ import BGM7441 from './BGM7441.jpg'
 
 import aa156344425744520DBE0505zbmuseum from './aa156344425744520DBE0505zbmuseum.jpg'
 
-import Baumhaus1 from './Baumhaus1.jpg'
+import baumhaus1 from './baumhaus1.jpg'
 import aa156344483710820DZH0101sblucahaennifront from './aa156344483710820DZH0101sblucahaennifront.jpg'
 import aa190225152733BAZ2002BILMobbing from './aa190225152733BAZ2002BILMobbing.jpg'
 import aa20190605Rammstein002 from './aa20190605Rammstein002.jpg'
 import MOS16261 from './MOS16261.jpg'
 import aa20190716BaeckereiAegerter003 from './aa20190716BaeckereiAegerter003.jpg'
 import chp3563 from './chp3563.jpg'
-import VisuOberburgKreiselNordWeb from './VisuOberburgKreiselNordWeb.jpg'
+import oberburg from './oberburg.jpg'
 import aa156353151192520DBE0707sbBaerenCombo2 from './aa156353151192520DBE0707sbBaerenCombo2.jpg'
 import chp3026 from './chp3026.jpg'
+import spring from './spring.jpg'
+import chp716 from './chp716.jpg'
+import waffen from './waffen.jpg'
 
-export default {MOS41361, RAH0760, BGM7441, aa156344425744520DBE0505zbmuseum, Baumhaus1, aa156344483710820DZH0101sblucahaennifront , aa190225152733BAZ2002BILMobbing, aa20190605Rammstein002, MOS16261,aa20190716BaeckereiAegerter003, aa156353151192520DBE0707sbBaerenCombo2, chp3563, chp3026, VisuOberburgKreiselNordWeb};
+export default {MOS41361, RAH0760, BGM7441, aa156344425744520DBE0505zbmuseum, baumhaus1, aa156344483710820DZH0101sblucahaennifront , aa190225152733BAZ2002BILMobbing, aa20190605Rammstein002, MOS16261,aa20190716BaeckereiAegerter003, aa156353151192520DBE0707sbBaerenCombo2, chp3563, chp3026, oberburg, spring, chp716, waffen};
